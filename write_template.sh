a1ae84237bff71c79419392c4ebb47dcbf7e8bb7
James Williams <jamesleighwilliams@gmail.com>
2020-09-24 15:51:17 +0100

    update script to generate random server name

--- write_template.sh
+++ write_template.sh
@@ -11,4 +11,8 @@
 set -euo allexport
 
 . ./.env || (echo 'You must create a .env file in this directory, see README.md' 2>&1 && exit 1)
+
+# obfuscate server name unless provided explicitly
+SERVER_NAME="${SERVER_NAME:-$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 10 ; echo '')}"
+
 envsubst < config/traefik-template.yml > config/traefik.yml
